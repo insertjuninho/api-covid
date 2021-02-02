5dc73984c801c4037981c336d7379483767e7a10
Juninho <juniorsax3@gmail.com>
2021-02-01 21:33:13 -0300

    #03: TDD

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -4,7 +4,20 @@ exports.index = (req,res) => {
 
 request('http://api.covidtracking.com/v1/status.json', function (error, response, body) {
 
+<<<<<<< Updated upstream
     var res = JSON.parse(body)
     console.log('body:', res); 
 });
 }
+=======
+    let res  = JSON.parse(body)
+    let count;
+    let val;
+    for (let i = count; i < val; i++) {
+         var result = i * res.runNumber
+            console.log("DAY = "+i + ":", result)
+        }
+  });
+}
+
+>>>>>>> Stashed changes
